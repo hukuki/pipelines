12f6fc080dfdbb06e53e73d45a2f1ae6cda43fe2
erenyenigul <tc.eren.yenigul@gmail.com>
2023-08-22 17:04:04 +0200

    feat: index for maps.

--- src/pipeline/base/map.ts
+++ src/pipeline/base/map.ts
@@ -1,16 +1,17 @@
 import { Pipeable } from "..";
 
 class Map<InputType, OutputType> extends Pipeable<InputType, OutputType> {
-    private map: (prev: InputType)=>OutputType;
+    private map: (prev: InputType, index? : number)=>OutputType;
+    private index: number = 0;
 
-    constructor(map: (prev: InputType)=>OutputType) {
+    constructor(map: (prev: InputType, index? : number)=>OutputType) {
         super();
 
         this.map = map;
     }
 
     public async run(prev: InputType): Promise<any> {
-        const out : OutputType = await this.map(prev);
+        const out : OutputType = await this.map(prev, this.index++);
 
         await this.next?.run(out);
     }
